51c94c6f50b6a210a7a0df924f61ae8437956489
TlpAyn <talipayan6035@gmail.com>
2023-03-10 09:14:33 +0100

    ilk 10 soru tmm

--- src/_08_03_2023_Selenium_2/_08_03_2023_SeleniumOdev2.java
+++ src/_08_03_2023_Selenium_2/_08_03_2023_SeleniumOdev2.java
@@ -1,6 +1,7 @@
 package _08_03_2023_Selenium_2;
 
 import Utility.BaseDriver;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -41,7 +42,7 @@ public class _08_03_2023_SeleniumOdev2 extends BaseDriver {
     public void Test3(){
         driver.get("https://testpages.herokuapp.com/styled/index.html");
 
-        WebElement Playground  = driver.findElement(By.id("Find By Playground"));
+        WebElement Playground  = driver.findElement(By.linkText("Find By Playground"));
         Playground.click();
 
         WebElement p5  = driver.findElement(By.id("p5"));
@@ -83,6 +84,58 @@ public class _08_03_2023_SeleniumOdev2 extends BaseDriver {
 
         System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());//input[@value='Search']
 
+    }
+    @Test
+    public void Test6(){
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+
+        WebElement fakeAlerts  = driver.findElement(By.id("fakealerttest"));
+        fakeAlerts.click();
+
+        WebElement p5  = driver.findElement(By.id("fakealert"));
+        p5.click();
+
+        System.out.println(p5.getText());
+
+        WebElement OK_Butonu  = driver.findElement(By.id("dialog-ok"));
+        OK_Butonu.click();
+
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());//input[@value='Search']
+
+    }
+    @Test
+    public void Test7(){
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+
+        WebElement fakeAlerts  = driver.findElement(By.id("fakealerttest"));
+        fakeAlerts.click();
+
+        WebElement p5  = driver.findElement(By.id("modaldialog"));
+        p5.click();
+
+        System.out.println(p5.getText());
+
+        WebElement OK_Butonu  = driver.findElement(By.id("dialog-ok"));
+        OK_Butonu.click();
+
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());//input[@value='Search']
+
+    }
+    @Test
+    public void Test8(){
+        driver.get("http://demo.seleniumeasy.com/bootstrap-alert-messages-demo.html");
+
+        WebElement NormalSuccseButton  = driver.findElement(By.id("normal-btn-success"));
+        NormalSuccseButton.click();
+
+
+        WebElement xButton  = driver.findElement(By.xpath(" //div[@class='alert alert-success alert-normal-success'] //button"));
+        xButton.click();
+
+
+        WebElement GreenBox  = driver.findElement(By.xpath("//div[@class='alert alert-success alert-normal-success']"));
+
+        Assert.assertFalse(GreenBox.isDisplayed());
     }
 
 }
